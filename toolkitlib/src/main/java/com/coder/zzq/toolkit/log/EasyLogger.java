273425bc7d4091f22f9215fd083360157834e612
zhiqiang.zhu <zhiqiang.zhu@partner.bmw.com>
2019-08-14 17:05:01 +0800

    剥离一些标志信息

--- toolkitlib/src/main/java/com/coder/zzq/toolkit/log/EasyLogger.java
+++ toolkitlib/src/main/java/com/coder/zzq/toolkit/log/EasyLogger.java
@@ -2,32 +2,19 @@ package com.coder.zzq.toolkit.log;
 
 import android.util.Log;
 
-import com.coder.zzq.toolkit.Utils;
+import com.coder.zzq.toolkit.Toolkit;
 
 public class EasyLogger {
-    public static final String DEFAULT_LOG_TAG = "easy-logger";
-    private static String sLogTag = DEFAULT_LOG_TAG;
-    private static boolean sPrintLog = false;
-
-    public static void setLogTag(String tag) {
-        sLogTag = Utils.isEmpty(tag) ? DEFAULT_LOG_TAG : tag;
-    }
-
-
-    public static void setPrintLog(boolean printLog) {
-        sPrintLog = printLog;
-    }
-
 
     public static void d(String msg) {
-        if (sPrintLog) {
-            Log.d(sLogTag, msg);
+        if (Toolkit.isEnablePrintLog()) {
+            Log.d(Toolkit.getLogTag(), msg);
         }
     }
 
     public static void e(String error) {
-        if (sPrintLog) {
-            Log.e(sLogTag, error);
+        if (Toolkit.isEnablePrintLog()) {
+            Log.e(Toolkit.getLogTag(), error);
         }
     }
 }
